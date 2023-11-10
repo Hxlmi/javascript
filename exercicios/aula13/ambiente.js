910566c393c16ba377cb201e3bc1e1d74a0e8c43
Paulo Vinicius Valverde dos Santos <91474572+Hxlmi@users.noreply.github.com>
2023-11-10 10:19:52 -0300

    aula13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,28 @@
+let num = 0;
+let index = 0;
+console.log(num);
+
+while(index < 6){
+    num++
+    index++
+    console.log(num);
+
+}
+// NumFinal = 10 | index = 10
+
+do{
+    num++
+    index++
+    console.log(num)
+    console.log(`valor de ${index}`)
+}while(index <= 5)
+// numFinal = 11 | index = 11
+
+for(let i = 0;i <= 1; i++){
+    num++
+    console.log(num);
+}
+/* 
+index é definido com 0
+
+*/
